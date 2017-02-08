5ae33f8cd9ca03af762e75b311d88dae87720052
Lida He <lida.he@dell.com>
2017-02-08 17:53:33 -0500

    modify the env variable to take a set of logstash destinations

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Render config file
-cat filebeat.yml | sed "s|LOGSTASH_HOST|${LOGSTASH_HOST}|g" | sed "s|LOGSTASH_PORT|${LOGSTASH_PORT}|g" > filebeat.yml.tmp
+cat filebeat.yml | sed "s|LOGSTASH_HOSTS|${LOGSTASH_HOSTS}|g" | sed "s|ELASTICSEARCH_HOSTS|${ELASTICSEARCH_HOSTS}|g > filebeat.yml.tmp
 cat filebeat.yml.tmp > filebeat.yml
 rm filebeat.yml.tmp
 
