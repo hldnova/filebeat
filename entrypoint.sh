68e90ad3cd875715e5c657b1087c8ab43bf3cd28
hldnova <hldnova@users.noreply.github.com>
2017-02-15 11:28:14 -0500

    Use temp file to keep original config file.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,6 @@
 set -e
 
 # Render config file
-cat filebeat.yml | sed "s|LOGSTASH_HOST|${LOGSTASH_HOST}|g" | sed "s|LOGSTASH_PORT|${LOGSTASH_PORT}|g" > filebeat.yml.tmp
-cat filebeat.yml.tmp > filebeat.yml
-rm filebeat.yml.tmp
+cat filebeat.yml.tmp | sed "s|LOGSTASH_HOST|${LOGSTASH_HOST}|g" | sed "s|LOGSTASH_PORT|${LOGSTASH_PORT}|g" > filebeat.yml
 
 exec "$@"
